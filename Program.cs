9599ed3272e7e680869d99697ab432cc6089390b
Russ Mehring <russ.mehring@zayo.com>
2021-07-01 17:05:34 -0600

    master - next module -- streaming large data

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-// using System.Linq;
-using linq.features;
+using System.Linq;
+// using linq.features;
+using Cars;
 
 namespace linq
 {
@@ -10,21 +11,41 @@ namespace linq
     {
         static void Main(string[] args)
         {
-            Employee[] devs = new Employee[] {
-                new Employee(){Id = 1, Name = "Scott"},
-                new Employee(){Id = 2, Name = "Chris"}
-            };
+            var cars = ProcessFile(@"fuel.csv");
+            var query = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016).OrderByDescending(c => c.Combined).ThenBy(c => c.Name).Take(10);
 
-            IEnumerable<Employee> sales = new List<Employee>(){
-                new Employee(){Id = 3, Name = "Alex"}
-            };
-
-            Console.WriteLine($"count: {sales.Count()}");
-
-            foreach(var person in sales) {
-                Console.WriteLine($"{person.Name} : {person.Id}");
+            foreach(var car in query){
+                Console.WriteLine($"{car.Manufacturer} : {car.Name} : {car.Combined}");
             }
         }
+
+        private static List<Car> ProcessFile(string path) {
+
+            return File.ReadAllLines(path)
+            .Skip(1)
+            .Where(line => line.Length > 1)
+            .ToCar()
+            .ToList();
+        }
+
+
+
+        // private void runLinqFeatures() {
+        //     Employee[] devs = new Employee[] {
+        //         new Employee(){Id = 1, Name = "Scott"},
+        //         new Employee(){Id = 2, Name = "Chris"}
+        //     };
+
+        //     IEnumerable<Employee> sales = new List<Employee>(){
+        //         new Employee(){Id = 3, Name = "Alex"}
+        //     };
+
+        //     Console.WriteLine($"count: {sales.Count()}");
+
+        //     foreach(var person in sales) {
+        //         Console.WriteLine($"{person.Name} : {person.Id}");
+        //     }
+        // }
 
         // private void executelinqvsnonlinqexample() {
         //     var path = @"../..//Desktop/";
@@ -63,5 +84,24 @@ namespace linq
         //         return y.Length.CompareTo(x.Length);
         //     }
         // }
+    }
+
+    public static class CarExtensions {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source) {
+            foreach(var line in source) {
+                var columns = line.Split(',');
+
+                yield return new Car {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
+        }
     }
 }
